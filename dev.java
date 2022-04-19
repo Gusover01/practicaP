f484e2d9b76e24ca239deaa6cf14cb67a4be47ed
Gustavo Del Angel <gusover01@localhost.localdomain>
2022-04-19 14:37:35 -0500

    metodos bidimencionales

--- dev.java
+++ dev.java
@@ -1,5 +1,9 @@
+import javax.print.attribute.SupportedValuesAttribute;
+
 public class dev {
 public static void main(String[] args) {
 
+    System.out.println("Implementando matrices bidimencionales en sistema.");
+    
     }
 }
