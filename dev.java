fb7af60aa81d9180b05214901ac72fb2628e6bc4
Gustavo Del Angel <gusover01@localhost.localdomain>
2022-04-20 16:01:48 -0500

    Se generararan cien lineas

--- dev.java
+++ dev.java
@@ -10,7 +10,7 @@ Scanner entrada = new Scanner(System.in);
     System.out.println("Implementando matrices bidimencionales en sistema.");
     System.out.println("****add autor****");
 
-    for(int i = 0 ; i<10 ; i++){
+    for(int i = 1 ; i<100 ; i++){
 
             System.out.println("Implemento de ciclo en System");
     
