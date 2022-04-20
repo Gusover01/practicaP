246c7c2392a48f908e3b5346da1f8ae10106d01b
Gustavo Del Angel <gusover01@localhost.localdomain>
2022-04-20 14:56:40 -0500

    Ciclo en sistema incluido.

--- dev.java
+++ dev.java
@@ -2,7 +2,13 @@ public class dev {
 public static void main(String[] args) {
     
     System.out.println("Implementando matrices bidimencionales en sistema.");
-    System.out.println("/****add autor****");
+    System.out.println("****add autor****");
+
+    for(int i = 0 ; i<10 ; i++){
+
+            System.out.println("Implemento de ciclo en System");
+    
+        }
 
 
     }
