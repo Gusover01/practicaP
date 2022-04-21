cbb7afb4f631b7f6b383461d372c6e921bba2368
Gustavo D. Angel <gdelangelf2100@alumno.ipn.mx>
2022-04-20 20:55:04 -0500

    una version del codigo

--- dev.java
+++ dev.java
@@ -9,7 +9,7 @@ Scanner entrada = new Scanner(System.in);
 
     System.out.println("Implementando matrices bidimencionales en sistema.");
     System.out.println("****add autor****");
-
+ System.out.println("****add autor en github****");
     for(int i = 1 ; i<100 ; i++){
 
             System.out.println("Implemento de ciclo en System");
