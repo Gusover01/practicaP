d44109687f595c2afb186a29fc5c5846695d8072
Gustavo D. Angel <gdelangelf2100@alumno.ipn.mx>
2022-04-22 17:56:16 -0500

    Formulario con error java

---
+++ Formulario.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+import java.awt.LayoutManager;
+import java.util.Scanner;
+
+public class Formulario extends JFrame {
+
+    public Formulario (){
+        setLayout(null);
+        label1 = new JLabel("Hola");
+        label1.setBounds(10,20,200,300);        
+        add(label1);
+    }
+
+    public static void main(String[] args) {
+        
+        Formulario formulario1 = new Formulario();
+        formulario1.setBounds(0,0,400,300);
+        formulario1.setVisible(true);
+        formulario1.setLocationRelativeTo(null);
+    }
+}
